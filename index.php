03021751630493f8c3f629a83f775bde6da553c8
59160816Sarocha <59160816@go.buu.ac.th>
2018-11-09 20:57:35 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<form action="/data.php" method="post">
+    student id
+    <input type="text" name="id" value="" placeholder="������ҧ: 59160001"><br><br>
+    firstname - lastname
+    <input type="text" name="firstname" value="" placeholder="������ҧ: �����">
+    <input type="text" name="lastname" value="" placeholder="������ҧ: �ѡ���¹"><br><br>
+    birthdate
+    <input type="date" name="birthday" value="" placeholder="mm/dd/yyyy"><br><br>
+    gender
+    <input type="radio" name="gender" value="˭ԧ">˭ԧ<br>
+    <input type="radio" name="gender" value="���">���<br>
+    knowledge
+    <input type="checkbox" name="exp1" value="�ҹ������">�ҹ������<br>
+    <input type="checkbox" name="exp2" value="�Ѳ�������">�Ѳ�������<br>
+    <input type="checkbox" name="exp3" value="���͢���">���͢���<br>
+    <input type="checkbox" name="exp4" value="����ͧ������">����ͧ������<br>
+    <input type="checkbox" name="exp5" value="��������">��������<br>
+    <input type="checkbox" name="exp6" value="�Ѵ����ç���">�Ѵ����ç���<br>
+    faculty-major
+    <select name="major">
+       <option>��س����͡�Ң� - ���</option>
+       <option>����Է�ҡ�����ʹ�� - ෤��������ʹ��</option>
+       <option>����Է�ҡ�����ʹ�� - �Է�ҡ�ä���������</option>
+       <option>����Է�ҡ�����ʹ�� - ���ǡ����Ϳ������</option>
+    </select><br><br>
+    demo
+    <textarea name="comment" rows="1" cols="50"></textarea><br>
+    <input type="submit" value="submit">
+    </form>
+    
